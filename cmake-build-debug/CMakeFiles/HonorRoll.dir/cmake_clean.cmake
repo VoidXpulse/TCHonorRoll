c25f6b533d1ecec6cc99a737ca6a5840412fad71
voidxpulse@gmail.com <voidxpulse@gmail.com>
2019-10-28 10:41:12 -0400

    //First

---
+++ cmake-build-debug/CMakeFiles/HonorRoll.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HonorRoll.dir/main.cpp.obj"
+  "HonorRoll.exe"
+  "HonorRoll.exe.manifest"
+  "HonorRoll.lib"
+  "HonorRoll.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HonorRoll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HonorRoll.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HonorRoll.
+# This may be replaced when dependencies are built.
